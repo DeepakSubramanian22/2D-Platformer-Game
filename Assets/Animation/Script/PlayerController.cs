cbb8ec91813cbbaf504411afc10a85959cd82cb2
Deeapk Subramanian <dee.subramanian@gmail.com>
2026-04-09 15:23:12 +0530

    Added Script

---
+++ Assets/Animation/Script/PlayerController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Awake()
+    {
+        Debug.Log("Player controller Awake");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale; 
+    }
+    } 
