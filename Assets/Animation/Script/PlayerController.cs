fbfebb5f339480d9fc040bebbb3e912a8952e769
Deeapk Subramanian <dee.subramanian@gmail.com>
2026-04-09 19:51:56 +0530

    Added-NewScript

--- Assets/Animation/Script/PlayerController.cs
+++ Assets/Animation/Script/PlayerController.cs
@@ -2,33 +2,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
-
-    private void Awake()
-    {
-        Debug.Log("Player controller Awake");
-    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    private void Update()
+    void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
         
-        Vector3 scale = transform.localScale;
-        if (speed < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        }
-        else if (speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-
-        transform.localScale = scale; 
     }
-    } 
+}
