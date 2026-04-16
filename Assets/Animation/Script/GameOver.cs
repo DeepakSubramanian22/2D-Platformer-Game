62932f2c1c89bff9948838157c841ae452d613e6
Deeapk Subramanian <dee.subramanian@gmail.com>
2026-04-16 11:20:21 +0530

    GameOver-Completed
    added the quit button and menu menu button in the game over

--- Assets/Animation/Script/GameOver.cs
+++ Assets/Animation/Script/GameOver.cs
@@ -7,10 +7,14 @@ public class GameOver : MonoBehaviour
 {
 
    public Button buttonRestart;
+   public Button buttonQuit;  
+   public Button buttonMainMenu;
 
    private void Awake()
    {
-      buttonRestart.onClick.AddListener(ReloadLevel);
+      buttonRestart.onClick.AddListener(ReloadLevel); 
+      buttonQuit.onClick.AddListener(quitGame);    
+      buttonMainMenu.onClick.AddListener(mainMenu);
    }
    public void PlayerDied()
    {
@@ -21,5 +25,14 @@ public class GameOver : MonoBehaviour
    {
       Debug.Log("Reload Scene 0");
       SceneManager.LoadScene(1); 
+   }
+
+   private void quitGame()
+   {
+      UnityEditor.EditorApplication.isPlaying = false; 
+   }
+   private void mainMenu()
+   {
+      SceneManager.LoadScene(0);
    }
 }
