8144b3371e055e3364f737696f3c3a7ffe7204f1
Deeapk Subramanian <dee.subramanian@gmail.com>
2026-04-16 10:43:03 +0530

    Added-QuitButton

--- Assets/Animation/Script/LobbyController.cs
+++ Assets/Animation/Script/LobbyController.cs
@@ -5,14 +5,22 @@ using UnityEngine.UI;
 public class LobbyController : MonoBehaviour
 {
     public Button Buttonplay;
+    public Button ButtonQuit;
 
     private void Awake()
     {
         Buttonplay.onClick.AddListener(PlayGame);
+        ButtonQuit.onClick.AddListener(QuitGame);
     }
 
     private void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    private void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false; 
+    }
+
 }
